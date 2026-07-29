18dfd3f1446d0c8cd9a0f168a3130dcd61454a0e
adiaug43 <adityababu968@gmail.com>
2026-07-29 07:22:53 +0530

    update js/productScript.js

--- js/productScript.js
+++ js/productScript.js
@@ -144,15 +144,3 @@ document.addEventListener('keydown', function(e) {
         if (e.key === "Escape") closeViewer();
     }
 });
-//loder for error image
-document.addEventListener("DOMContentLoaded", () => {
-  // Website ki saari images ko select karein
-  const images = document.querySelectorAll("img");
-
-  images.forEach((img) => {
-    img.addEventListener("error", function () {
-      // Jab image load na ho, tab default image set karein
-      this.src = "../assets/thumbnails/kits/loading.gif"; // Yahan apni default image ka URL/path dein
-    });
-  });
-});
